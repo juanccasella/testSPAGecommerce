b85f0e9e552dcda4be07e61f3bc0827428a909e0
Juan Cruz Casella <juan.casella@DASLOCAL.gob>
2023-10-17 16:51:42 -0300

    Promises

--- src/components/ItemListContainer/ItemListContainer.jsx
+++ src/components/ItemListContainer/ItemListContainer.jsx
@@ -1,10 +1,31 @@
 import './styles.module.css'
+import { useEffect, useRef, useState } from 'react'
 
-const ItemListContainer = ({greeting}) => {
+const ItemListContainer = ({props}) => {
+    const [productos, setProducts] = useState([]);
+
+    const promesa = new Promise((resolve,reject)=>{
+        const productosArray = [
+            {name: "Cuchillo", precio: 15000, id:1},
+            {name: "Olla", precio: 20000, id:2},
+            {name: "Tabla", precio: 5000, id:3},
+            {name: "Batidor", precio: 2500, id:4},
+            {name: "Afilador", precio: 8000, id:5},
+        ]
+        setTimeout (() =>{
+            productosArray.length > 0 ? resolve(productosArray) : reject({data: [], message: "No hay stock disponible"})
+        }, 4000)
+    })
     
+    useEffect(() => {
+        promesa
+        .then(res => console.table(res))
+        .catch(error => console.error(error.message))
+    }, []);
+
     return(
         <>
-        <h2>{greeting}</h2>
+            <h2>{props}</h2>
         </>
     )
 }
