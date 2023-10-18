1acbaf5a818e539103215bb0f02e0c149fa5c886
Juan Cruz Casella <juan.casella@DASLOCAL.gob>
2023-10-18 16:50:33 -0300

    cositas

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,18 @@
-import { useState } from 'react'
-import ItemListContainer from './components/itemListContainer/itemListContainer'
-import NavBar from './components/navBar/NavBar'
+import { useState } from "react";
+import ItemListContainer from "./components/itemListContainer/itemListContainer";
+import NavBar from "./components/navBar/NavBar";
 
 function App() {
+  const [mostrar, setMostrar] = useState(true);
+
   return (
     <>
-      <NavBar/>
-      <ItemListContainer greeting="Bienvenido a SPAG tienda online"/>
+      <NavBar />
+      {mostrar && (
+        <ItemListContainer greeting="Bienvenido a SPAG tienda online" />
+      )}
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/components/ItemListContainer/ItemListContainer.jsx
+++ src/components/ItemListContainer/ItemListContainer.jsx
@@ -1,17 +1,18 @@
 import './styles.module.css'
 import { useEffect, useRef, useState } from 'react'
 
-const ItemListContainer = ({props}) => {
-    const [productos, setProducts] = useState([]);
+const ItemListContainer = ({greeting}) => {
+    const [products, setProducts] = useState([])
 
-    const promesa = new Promise((resolve,reject)=>{
+    const promesa = new Promise((resolve,reject) => {
         const productosArray = [
-            {name: "Cuchillo", precio: 15000, id:1},
-            {name: "Olla", precio: 20000, id:2},
-            {name: "Tabla", precio: 5000, id:3},
-            {name: "Batidor", precio: 2500, id:4},
-            {name: "Afilador", precio: 8000, id:5},
+            {name: "Cuchillo", precio: 15000, id: 1},
+            {name: "Olla", precio: 20000, id: 2},
+            {name: "Tabla", precio: 5000, id: 3},
+            {name: "Batidor", precio: 2500, id: 4},
+            {name: "Afilador", precio: 8000, id: 5},
         ]
+
         setTimeout (() =>{
             productosArray.length > 0 ? resolve(productosArray) : reject({data: [], message: "No hay stock disponible"})
         }, 4000)
@@ -19,13 +20,18 @@ const ItemListContainer = ({props}) => {
     
     useEffect(() => {
         promesa
-        .then(res => console.table(res))
+        .then(res => {
+            console.table(res)
+            setProducts(res)
+        }) //Una vez que nos aseguremos de que el console esta OK, puedo sacar el console.log por el valor nuevo setProducts (BUENA PRACTICA)
         .catch(error => console.error(error.message))
-    }, []);
+    }, [])
 
     return(
         <>
-            <h2>{props}</h2>
+            <h2 className='saludo'>{greeting}</h2>
+            {products.map(prod => <p key={prod.id}>{prod.name} {prod.precio}</p> )}
+            {/* cualquier nombre (en este caso abreviacion de product) */}
         </>
     )
 }
