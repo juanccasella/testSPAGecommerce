6f1231baf8e017225407e16c9a959de37ce45929
Juan Cruz Casella <juan.casella@DASLOCAL.gob>
2023-11-21 16:37:05 -0300

    1css

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,16 +1,17 @@
 import CartWidget from "../cartWidget/cartWidget"
 import { NavLink } from 'react-router-dom'
+import "./navBar.css"
 
 
 const NavBar = () => {
     return(
 
         <div className='navbar'>
-            <NavLink to={'/products'}>SPAG tienda</NavLink>
+            <NavLink className='logo' to={'/products'}>SPAG tienda</NavLink>
             <NavLink to={"/products/electrodomésticos"}>Electrodomésticos</NavLink>
             <NavLink to={"/products/bazar"}>Bazar</NavLink>
             <NavLink to={"/products/cafeteras"}>Cafeteras</NavLink>
-            <NavLink to={"/products/cuchilleria"}>Cuchillería</NavLink>
+            <NavLink to={"/products/cuchillería"}>Cuchillería</NavLink>
             <CartWidget/>
         </div>
     )
